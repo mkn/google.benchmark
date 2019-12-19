3c2bab404d11ff11262cf3ac803e1dbd38959ccd
philip.deegan <philip.deegan@gmail.com>
2019-12-19 11:27:35 +0100

    updates

--- mkn.sh
+++ mkn.sh
@@ -1,20 +1,7 @@
-
-
-rm -rf g google
-
-git clone https://github.com/google/benchmark -b master g --recursive
-
-mkdir -p google/src/posix
-
-cp -r g/include google
-cp -r g/src google
-cp -r g/test google
-cp g/* google 2>&1 | grep -v "omitting directory"
-
-mv google/src/re_posix.cc google/src/posix/re_posix.cc
-
-rm google/app*
-rm google/CM*
-rm google/ming*
-
-rm -rf g
+#!/usr/bin/env bash
+set -e
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="g"
+GIT_URL="https://github.com/google/benchmark"
+VERSION="master"
+[ ! -d "$CWD/$DIR" ] && git clone --depth 1 $GIT_URL -b $VERSION $DIR --recursive
